f3a1c837677a5587e93721fd697df421415c764f
Alejandro <alerr415@hotmail.com>
2021-05-14 15:46:22 +0200

    first commit

---
+++ bleAdvertiser1/OnBoardView.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoardView.swift
+//  bleAdvertiser1
+//
+//  Created by SwiftApp on 2021. 05. 14..
+//
+
+import SwiftUI
+
+struct OnBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardView()
+    }
+}
